12f82e91d1d0b14b9353a839454307d0ea54dc87
Chitsanupak <pung66.com@gmail.com>
2026-01-31 01:04:47 +0700

    add  create accounts

--- app.mjs
+++ app.mjs
@@ -31,3 +31,27 @@ app.post("/customers-create",async (req,res) => {
 app.listen(port, () => {
     console.log(`runing at ${port}`)
 });
+
+
+app.post("/accounts_create", async (req,res)=> {
+    const newaccouts = {
+        ...req.body,
+        created_at: new Date(),
+        updated_at: new Date(),
+    };
+    await connectionPool.query(
+        `insert into accouts (account_id,customer_id ,account_number ,account_type ,balance ,status ,created_at )
+        values ($1,$2,$3,$4,$5,$6,$7)`,
+        [
+            1,
+            2,
+            newaccouts.account_number,
+            newaccouts.account_type,
+            newaccouts.balance,
+            newaccouts.status,
+            newaccouts.created_at,
+        ]
+        
+    );
+})
+
